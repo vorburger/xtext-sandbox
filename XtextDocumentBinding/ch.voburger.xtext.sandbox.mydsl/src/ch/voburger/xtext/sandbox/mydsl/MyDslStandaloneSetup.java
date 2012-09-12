c04307cdf4f87065b0eac267c67953638aa710ed
Michael Vorburger <mike@vorburger.ch>
2012-09-12 17:11:04 +0200

    New empty sample project as created by Xtext Wizard

---
+++ XtextDocumentBinding/ch.voburger.xtext.sandbox.mydsl/src/ch/voburger/xtext/sandbox/mydsl/MyDslStandaloneSetup.java
@@ -0,0 +1,14 @@
+
+package ch.voburger.xtext.sandbox.mydsl;
+
+/**
+ * Initialization support for running Xtext languages 
+ * without equinox extension registry
+ */
+public class MyDslStandaloneSetup extends MyDslStandaloneSetupGenerated{
+
+	public static void doSetup() {
+		new MyDslStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+}
+
