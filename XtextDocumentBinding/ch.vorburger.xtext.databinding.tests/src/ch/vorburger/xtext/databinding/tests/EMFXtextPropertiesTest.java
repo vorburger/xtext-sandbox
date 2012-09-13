57efbd224e525519e2839a7eaa0ee22202e774f6
Michael Vorburger <mike@vorburger.ch>
2012-09-13 15:44:33 +0200

    Test now working according to old (and bad approach) API

--- XtextDocumentBinding/ch.vorburger.xtext.databinding.tests/src/ch/vorburger/xtext/databinding/tests/EMFXtextPropertiesTest.java
+++ XtextDocumentBinding/ch.vorburger.xtext.databinding.tests/src/ch/vorburger/xtext/databinding/tests/EMFXtextPropertiesTest.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Michael Vorburger (http://www.vorburger.ch).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+/**
+ *  Copyright 2012 Michael Vorburger (http://www.vorburger.ch)
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package ch.vorburger.xtext.databinding.tests;
 
 import static org.junit.Assert.assertEquals;
@@ -5,20 +29,27 @@ import static org.junit.Assert.assertEquals;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.beans.BeanProperties;
 import org.eclipse.core.databinding.observable.Realm;
-import org.eclipse.emf.databinding.EMFProperties;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EDataType;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.xtext.resource.XtextResource;
 import org.junit.Test;
 
 import ch.vorburger.beans.AbstractPropertyChangeNotifier;
+import ch.vorburger.xtext.databinding.EMFXtextProperties;
 import ch.vorburger.xtext.databinding.XtextDataBindingContext;
 import ch.vorburger.xtext.databinding.tests.utils.DatabindingTestRealm;
 import ch.vorburger.xtext.databinding.tests.utils.ECoreHelper;
+import ch.vorburger.xtext.databinding.tests.utils.XtextResourceTestAccess;
 
+/**
+ * Tests for the XtexteDataBindingContext & XtextProperties.
+ * 
+ * @author Michael Vorburger
+ */
 public class EMFXtextPropertiesTest {
 
 	@SuppressWarnings("serial")
@@ -33,7 +64,7 @@ public class EMFXtextPropertiesTest {
 	}
 	
 	@Test
-	public void test1() {
+	public void testSimpleValueBinding() {
 		// Create an ECore model
 		ECoreHelper helper = new ECoreHelper();
 		EDataType stringType = EcorePackage.eINSTANCE.getEString();
@@ -51,9 +82,14 @@ public class EMFXtextPropertiesTest {
 		Realm realm = new DatabindingTestRealm();
 		DataBindingContext db = new XtextDataBindingContext(realm);
 		
+		// TODO Use WritableValue instead of direct eObject
+		
+		// TODO Do I really need to go through an XtextResource? Better abstraction?
+		XtextResource resource = new XtextResource();
+		resource.getContents().add(eObject);
+		XtextResourceTestAccess access = new XtextResourceTestAccess(resource);
 		db.bindValue(BeanProperties.value("name").observe(bean),
-				// TODO Use EMFXtextProperties instead!
-				EMFProperties.value(titleFeature).observe(eObject));
+				EMFXtextProperties.value(access, titleFeature).observe(eObject));
 		
 		assertEquals(eObject.eGet(titleFeature), bean.getName());
 		
---
+++ XtextDocumentBinding/ch.vorburger.xtext.databinding.tests/src/ch/vorburger/xtext/databinding/tests/utils/XtextResourceTestAccess.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Michael Vorburger (http://www.vorburger.ch).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package ch.vorburger.xtext.databinding.tests.utils;
+
+import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+
+import ch.vorburger.xtext.databinding.IXtextResourceReadWriteAccess;
+
+/**
+ * IXtextResourceReadWriteAccess for tests.
+ *
+ * @author Michael Vorburger
+ */
+public class XtextResourceTestAccess implements IXtextResourceReadWriteAccess {
+
+	private final XtextResource state;
+
+	public XtextResourceTestAccess(XtextResource state) {
+		super();
+		this.state = state;
+	}
+
+	@Override
+	public <T> T readOnly(IUnitOfWork<T, XtextResource> work) {
+		try {
+			return work.exec(state);
+		} catch (Exception e) {
+			throw new WrappedException(e);
+		}
+	}
+
+	@Override
+	public <T> T modify(IUnitOfWork<T, XtextResource> work) {
+		try {
+			return work.exec(state);
+		} catch (Exception e) {
+			throw new WrappedException(e);
+		}
+	}
+
+}
