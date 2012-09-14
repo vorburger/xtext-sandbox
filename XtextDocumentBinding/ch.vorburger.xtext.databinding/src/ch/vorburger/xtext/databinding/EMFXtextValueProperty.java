5d22d0cf9f3c0ced69b8e9fb611c35529c9718e2
Michael Vorburger <mike@vorburger.ch>
2012-09-14 14:33:13 +0200

    Contribution from Phani Kumar (@netsrujana) for correct adaptListener

--- XtextDocumentBinding/ch.vorburger.xtext.databinding/src/ch/vorburger/xtext/databinding/EMFXtextValueProperty.java
+++ XtextDocumentBinding/ch.vorburger.xtext.databinding/src/ch/vorburger/xtext/databinding/EMFXtextValueProperty.java
@@ -8,6 +8,10 @@
 
 package ch.vorburger.xtext.databinding;
 
+import org.eclipse.core.databinding.property.INativePropertyListener;
+import org.eclipse.core.databinding.property.IProperty;
+import org.eclipse.core.databinding.property.ISimplePropertyListener;
+import org.eclipse.emf.databinding.internal.EMFPropertyListener;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.resource.XtextResource;
@@ -20,8 +24,9 @@ import org.eclipse.xtext.util.concurrent.IWriteAccess;
  * Like EMFEditValueProperty, but using an IXtextResourceReadWriteAccess instead of an EditingDomain.
  *
  * @author Michael Vorburger
+ * @author Phani Kumar - Contributions for correct adaptListener
  */
-@SuppressWarnings("unchecked")
+@SuppressWarnings({ "unchecked", "restriction" })
 public class EMFXtextValueProperty extends EMFValuePropertyWithErrorLogging {
 
 	public EMFXtextValueProperty(EStructuralFeature eStructuralFeature) {
@@ -43,13 +48,77 @@ public class EMFXtextValueProperty extends EMFValuePropertyWithErrorLogging {
 	@Override
 	protected Object doGetValue(final Object source) {
 		IReadAccess<XtextResource> access = (IReadAccess<XtextResource>) source;
-		return access.readOnly(new IUnitOfWork<Object, XtextResource>() {
-			@Override public Object exec(XtextResource state) throws Exception {
+		EObject eObj = getEObject(access);
+		return EMFXtextValueProperty.super.doGetValue(eObj);
+	}
+	
+	/**
+	 * @param access
+	 * @return
+	 */
+	public EObject getEObject(IReadAccess<XtextResource> access) {
+		return access.readOnly(new IUnitOfWork<EObject, XtextResource>() {
+			@Override public EObject exec(XtextResource state) throws Exception {
 	    		// TODO Handling (via TDD) if it doesn't exist yet! Should probably return null and NOT create it on-the-fly?
-	    		EObject eObject = state.getContents().get(0);
-				return EMFXtextValueProperty.super.doGetValue(eObject);
+	    		return state.getContents().get(0);
+				
 			}
 		});
 	}
+
+	@Override
+	public INativePropertyListener adaptListener(final ISimplePropertyListener listener)  {
+		return new EMFPropertyListener.EMFValuePropertyListener() {
+			@Override
+			public void addTo(Object source) {
+				if (source != null) {
+					EObject eobj = null;
+					if (source instanceof EObject) {
+						eobj = (EObject) source;
+					} else if (source instanceof IReadAccess<?>) {
+						IReadAccess<XtextResource> access = (IReadAccess<XtextResource>) source;
+						eobj = (EObject) EMFXtextValueProperty.this.getEObject(access);
+					}
+					if (eobj != null) {
+						eobj.eAdapters().add(this);
+					}
+				}
+			}
+
+			@Override
+			public void removeFrom(Object source) {
+				if (source != null) {
+					EObject eobj = null;
+					if (source instanceof EObject) {
+						eobj = (EObject) source;
+					} else if (source instanceof IReadAccess<?>) {
+						IReadAccess<XtextResource> access = (IReadAccess<XtextResource>) source;
+						eobj = (EObject) EMFXtextValueProperty.this.getEObject(access);
+					}
+					if (eobj != null) {
+						eobj.eAdapters().remove(this);
+					}
+				}
+			}
+
+	    	@Override
+	        protected IProperty getOwner()
+	        {
+	          return EMFXtextValueProperty.this;
+	        }
+
+	        @Override
+	        protected ISimplePropertyListener getListener()
+	        {
+	          return listener;
+	        }
+
+			@Override
+	        protected EStructuralFeature getFeature()
+	        {
+	          return EMFXtextValueProperty.this.getFeature();
+	        }
+	      };
+	  }
 
 }
