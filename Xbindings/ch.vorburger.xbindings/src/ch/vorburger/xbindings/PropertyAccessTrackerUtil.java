8bdc5b67c6bbaaa39bfaa817eb41898f943a9fef
Michael Vorburger <mike@vorburger.ch>
2012-12-27 22:53:57 +0100

    Minor: Private field renamed, for clarity

--- Xbindings/ch.vorburger.xbindings/src/ch/vorburger/xbindings/PropertyAccessTrackerUtil.java
+++ Xbindings/ch.vorburger.xbindings/src/ch/vorburger/xbindings/PropertyAccessTrackerUtil.java
@@ -17,35 +17,35 @@ import org.eclipse.xtext.xbase.lib.Procedures.Procedure0;
  */
 public class PropertyAccessTrackerUtil { // TODO rename class.. what would be a better name?
 
-	private static final ThreadLocal<PropertyChangeListener> threadLocal = new ThreadLocal<>();
+	private static final ThreadLocal<PropertyChangeListener> currentThreadPropertyChangeListener = new ThreadLocal<>();
 
 	public static final PropertyAccessListener INSTANCE = new PropertyAccessListener() {
 		@Override
 		public void accessed(PropertyChangeNotifier cn) {
-			if (threadLocal.get() != null)
-				cn.addChangeListener(threadLocal.get());
+			if (currentThreadPropertyChangeListener.get() != null)
+				cn.addChangeListener(currentThreadPropertyChangeListener.get());
 		};
 	};
 
-	public static void bind(final Procedure0 assigner) {
-		if (threadLocal.get() != null) {
+	public static void bind(final Procedure0 binding) {
+		if (currentThreadPropertyChangeListener.get() != null) {
 			throw new IllegalStateException("how come threadLocal is already set?!");
 		}
 		
 		// PropertyAccessTrackerUtil::ThreadLocal.set( [| assigner.apply() ] )
-		threadLocal.set(new PropertyChangeListener() {
+		currentThreadPropertyChangeListener.set(new PropertyChangeListener() {
 			@Override
 			public void changed() {
-				assigner.apply();
+				binding.apply();
 			}
 		});
 		
 		try {
-			assigner.apply(); // initial assignment
+			binding.apply(); // initial assignment
 		// could catch(Throwable) & log in case of trouble with initial assignment, but as it nicely bubbles up, there is currently no need to do that
 		// if Exception is caught, could remove/dispose binding here - but we would have to keep track of all PropertyChangeNotifiers seen for that
 		} finally {
-			threadLocal.remove();
+			currentThreadPropertyChangeListener.remove();
 		}
 	}
 
