d87f1d23cd6aad823d710e5a307a5425c4787a15
Michael Vorburger <mike@vorburger.ch>
2013-08-22 10:38:28 +0200

    JavaDoc comments (no code changes) with references to EMF 2.9 performance bug

--- emfPerformanceCreateURI/src/ch/vorburger/emfperf/CreateURIPerformanceTest.java
+++ emfPerformanceCreateURI/src/ch/vorburger/emfperf/CreateURIPerformanceTest.java
@@ -9,8 +9,16 @@ import org.junit.Test;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
+/**
+ * Illustration of a serious performance issue in EMF 2.9.
+ * 
+ * @see http://www.eclipse.org/forums/index.php/m/1091579/
+ * @see https://bugs.eclipse.org/bugs/show_bug.cgi?id=415311
+ * 
+ * @author Michael Vorburger
+ */
 public class CreateURIPerformanceTest {
-
+	
 	@Test
 	public void testCreateURIPerformance() throws Exception {
 		URL url = Resources.getResource(this.getClass(), "mdfWithManyStrings.txt");
@@ -23,11 +31,12 @@ public class CreateURIPerformanceTest {
 			String localName = splitMdfName[1];
 			
 			// Performance VERY BAD
-			URI.createGenericURI("mdfname", fqMdfName, null);
+			//URI.createGenericURI("mdfname", fqMdfName, null);
 			
 			// Good Performance
 			//String[] segments = new String[] { domainName, localName };
-			//URI.createHierarchicalURI("mdfname", null, null, segments , null, null);
+			String[] segments = new String[] { fqMdfName };
+			URI.createHierarchicalURI("mdfname", null, null, segments , null, null);
 		}
 		long duration = System.currentTimeMillis() - startTime;
 		System.out.println(duration);
